b47509f6764e795b381bef1f23dace0394040201
Parmar Hetvi <hetvi9434@gamil.com>
2024-09-19 17:24:54 +0530

    Updates

---
+++ precache-manifest.7941aa07f423044c4ceb88186866025a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7332e185d9fcdcff8e67395440e95b7",
+    "url": "/src/index.html"
+  },
+  {
+    "revision": "a561e02fa0d8ee7f6ad9",
+    "url": "/src/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4a73fbe1344389c52726",
+    "url": "/src/static/js/2.819bb1f7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/src/static/js/2.819bb1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5c55aa457ee42deb7a",
+    "url": "/src/static/js/3.20a5957b.chunk.js"
+  },
+  {
+    "revision": "a561e02fa0d8ee7f6ad9",
+    "url": "/src/static/js/main.8cf6c568.chunk.js"
+  },
+  {
+    "revision": "5f38cdd20fe989444c32",
+    "url": "/src/static/js/runtime-main.4d364f6e.js"
+  }
+]);
